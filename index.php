d4b2f85a1514f00115ae6641535ff7ce98772b91
Lukas Velek <lukas.velek@centrum.cz>
2022-08-16 10:06:22 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="css/style.css">
+        
+        <meta charset="utf-8" name="CHARSET" content="UTF-8">
+        <meta name="AUTHOR" content="Lukas Velek">
+
+        <title></title>
+    </head>
+</html>
